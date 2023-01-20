e3dbb1ac6a3882f91aaa34192ef08a8ae7096bc0
Ejaz <mohammadejaz978@gmail.com>
2023-01-20 10:40:49 +0530

    api link changed

--- src/redux/saga-api/api.saga.js
+++ src/redux/saga-api/api.saga.js
@@ -1,23 +1,23 @@
 import axios from "axios";
 
 // 1. GET ALL USER DETIALS
-export const loaderUserDetails = async () => await axios.get("/user/getall");
+export const loaderUserDetails = async () => await axios.get("https://apicars.prisms.in/user/getall");
 
 // 2. CREATE USER
-export const postUser = async (user) => await axios.post("/user/create", user);
+export const postUser = async (user) => await axios.post("https://apicars.prisms.in/user/create", user);
 
 // 3. GET USER BY ID
-export const getUserId = async (id) => await axios.get(`/user/get/${id}`);
+export const getUserId = async (id) => await axios.get(`https://apicars.prisms.in/user/get/${id}`);
 
 // 4. ADD CAR DETAILS
-export const addCarInfo = async (data) => await axios.post("/car/create", data);
+export const addCarInfo = async (data) => await axios.post("https://apicars.prisms.in/car/create", data);
 
 // 5. GET CARS BY ID
-export const getCarById = async (id) => await axios.get(`/car/get/${id}`);
+export const getCarById = async (id) => await axios.get(`https://apicars.prisms.in/car/get/${id}`);
 
 // 6. GET ALL CARS FOR USER ID
 export const getAllUserCarById = async (userid) =>
-  await axios.get("/car/getall", userid, {
+  await axios.get("https://apicars.prisms.in/car/getall", userid, {
     header: {
       "Content-Type": "application/json",
     },
@@ -25,7 +25,7 @@ export const getAllUserCarById = async (userid) =>
 
 // 7. CREATE SERVICE FOR CAR
 export const createServices = async (data) =>
-  await axios.post("/servicing/create", data);
+  await axios.post("https://apicars.prisms.in/servicing/create", data);
 
 // 8. GET ALL SERVICES RECORDS FOR CAR
 export const ServicesRecord = async (id) => {
@@ -34,7 +34,7 @@ export const ServicesRecord = async (id) => {
   };
   await axios({
     method: "GET",
-    url: "/servicing/getall",
+    url: "https://apicars.prisms.in/servicing/getall",
     data: res,
     headers: {
       "Content-Type": "application/json",
